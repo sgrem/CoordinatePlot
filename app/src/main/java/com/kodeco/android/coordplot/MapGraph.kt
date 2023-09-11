040a843ea8839de3faddad6368f0cde842417501
Steve Gremban <steve.gremban@gmail.com>
2023-09-11 13:38:11 -0500

    Change Theme colors and replace cursor pointer with an arrow image

--- app/src/main/java/com/kodeco/android/coordplot/MapGraph.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MapGraph.kt
@@ -1,30 +1,37 @@
 package com.kodeco.android.coordplot
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 @Composable
 fun MapGraph(xPercent: Float, yPercent: Float, mapSize: Int, cursorSize: Int) {
+    val cursorResource = painterResource(id = R.drawable.arrow)
     Box(modifier = Modifier
         .size((mapSize + cursorSize).dp)
         .background(Color.LightGray)) {
-        Box(
+        Image(painter = cursorResource,
+            contentDescription = "Cursor",
+            Modifier
+                .size(cursorSize.dp)
+                .offset((xPercent * mapSize).dp, (yPercent * mapSize).dp)
+        )
+/*        Box(
             modifier = Modifier
                 .size(cursorSize.dp)
                 .offset((xPercent * mapSize).dp, (yPercent * mapSize).dp)
                 .clip(CircleShape)
                 .background(Color.Blue)
-        )
+        )*/
     }
 }
 
--- app/src/main/java/com/kodeco/android/coordplot/ui/theme/Color.kt
+++ app/src/main/java/com/kodeco/android/coordplot/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.kodeco.android.coordplot.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green80 = Color(0xFFACF05D)
+val GreenGrey = Color(0xFFCFE6B4)
+val Yellow80 = Color(0xFFECF0B5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green40 = Color(0xFF205821)
+val GreenGrey40 = Color(0xFF506851)
+val Yellow40 = Color(0xFF837B33)
