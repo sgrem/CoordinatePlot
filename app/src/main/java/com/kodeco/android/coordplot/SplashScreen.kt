2aae1c319de7b2f514a014466eb45952e151de37
Steve Gremban <steve.gremban@gmail.com>
2023-09-12 09:40:57 -0500

    Add styling to splash screen

--- app/src/main/java/com/kodeco/android/coordplot/SplashScreen.kt
+++ app/src/main/java/com/kodeco/android/coordplot/SplashScreen.kt
@@ -1,22 +1,45 @@
 package com.kodeco.android.coordplot
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen() {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
         contentAlignment = Alignment.Center
     ) {
-        Text("Welcome to the Coordinate Plot App")
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                "Welcome to the",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                "Coordinate Plot App",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }
 
@@ -27,5 +50,13 @@ fun SplashScreenWithNavigation(navController: NavController) {
         navController.navigate("main_screen")
     }
     SplashScreen()
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview(){
+    MyApplicationTheme {
+        SplashScreen()
+    }
 }
 
