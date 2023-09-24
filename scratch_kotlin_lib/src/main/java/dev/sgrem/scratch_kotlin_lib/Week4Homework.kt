2864174b4125a87c3e9725b1eedd846b88458ed0
Steve Gremban <steve.gremban@gmail.com>
2023-09-23 20:59:34 -0500

    Assignment #2 + #3: Create a Task list and a function to add a task to the list

--- scratch_kotlin_lib/src/main/java/dev/sgrem/scratch_kotlin_lib/Week4Homework.kt
+++ scratch_kotlin_lib/src/main/java/dev/sgrem/scratch_kotlin_lib/Week4Homework.kt
@@ -1,14 +1,21 @@
 package dev.sgrem.scratch_kotlin_lib
 
-fun main(){
+fun main() {
     println("Hello world!")
+
+    // 1. Task Data:
+    data class Task(
+        val title: String,
+        var priority: Int,
+        var completed: Boolean
+    )
+
+    // 2. Task List
+    val taskList: MutableList<Task> = mutableListOf()
+
+    // 3. Add Task:
+    fun addTask(title: String, priority: Int, completed: Boolean){
+        taskList.add(Task(title, priority, completed))
+    }
 }
-
-// 1. Task Data:
-data class Task(
-    val title: String,
-    var priority: Int,
-    var completed: Boolean
-)
-
 
